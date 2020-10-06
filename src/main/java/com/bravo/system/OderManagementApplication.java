ce34ecfe034546faa153fcd9fa86b48da9c34e7b
Tharindu Wickramanayake <36078972+tdwic@users.noreply.github.com>
2020-10-06 19:51:28 +0530

    Initaal commit

---
+++ src/main/java/com/bravo/system/OderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.bravo.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OderManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OderManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bravo/system/OderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bravo.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OderManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
