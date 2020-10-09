e8e20026b7011222e30eef992b5ebb1311bd72de
Tharindu Wickramanayake <36078972+tdwic@users.noreply.github.com>
2020-10-09 20:31:03 +0530

    user class created

--- src/main/java/com/bravo/system/model/User.java
+++ src/main/java/com/bravo/system/model/User.java
@@ -3,25 +3,61 @@ package com.bravo.system.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document (collection = "test")
+@Document (collection = "User")
 public class User {
     @Id
-    private String testID;
-    private String testName;
+    private String id;
+    private String fName;
+    private String lName;
+    private String email;
+    private String password;
+    private String role;
 
-    public String getTestID() {
-        return testID;
+    public String getId() {
+        return id;
     }
 
-    public void setTestID(String testID) {
-        this.testID = testID;
+    public void setId(String id) {
+        this.id = id;
     }
 
-    public String getTestName() {
-        return testName;
+    public String getfName() {
+        return fName;
     }
 
-    public void setTestName(String testName) {
-        this.testName = testName;
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
