ef4f44596adc87ea5efe1c562b6eec2a65f7dc5b
YuvinR <yuvinransika1998@gmail.com>
2020-10-17 20:46:44 +0530

    ordercontroller changes

---
+++ src/main/java/com/bravo/system/model/OrderReurnModel.java
@@ -0,0 +1,16 @@
+package com.bravo.system.model;
+
+public class OrderReurnModel {
+    private String OrderNo;
+
+    public String getOrderNo() {
+
+        return OrderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        OrderNo = orderNo;
+    }
+
+
+}
